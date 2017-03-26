8668ee5d44f68a2eeede18e245ba7205cca847d9
Tairan Wang <tairan.wang@gmail.com>
2017-03-26 20:35:29 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# electron template
+
+## quick start
---
+++ main.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const electron = require('electron')
+
+const BrowserWindow = electron.BrowserWindow
+const app = electron.app
+
+var mainWindow = null
+
+function initialize() {
+  var shouldQuit = makeSingleInstance()
+
+  if (shouldQuit) return app.quit()
+
+  function createWindow() {
+    var windowOptions = {
+      width: 1080,
+      minWidth: 680,
+      height: 840,
+      title: app.getName()
+    }
+
+    mainWindow = new BrowserWindow(windowOptions)
+    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+    mainWindow.on('closed', function() {
+      mainWindow = null
+    })
+  }
+
+  app.on('ready', function() {
+    createWindow()
+  })
+
+  app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', function() {
+    if (mainWindow === null) {
+      createWindow()
+    }
+  })
+}
+
+
+
+// Make this app a single instance app.
+//
+// The main window will be restored and focused instead of a second window
+// opened when a person attempts to launch a second instance.
+//
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance() {
+  if (process.mas) return false
+
+  return app.makeSingleInstance(function() {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
+
+// Handle Squirrel on Windows startup events
+switch (process.argv[1]) {
+  case '--squirrel-install':
+    autoUpdater.createShortcut(function() {
+      app.quit()
+    })
+    break
+  case '--squirrel-uninstall':
+    autoUpdater.removeShortcut(function() {
+      app.quit()
+    })
+    break
+  case '--squirrel-obsolete':
+  case '--squirrel-updated':
+    app.quit()
+    break
+  default:
+    initialize()
+}
